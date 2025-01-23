c4381fb449d7bf25811ae6fb69059219f98a25c1
Srinidhik15 <nidhisrikankanala@gmail.com>
2025-01-23 14:03:18 -0600

    docker cicd yml file is ready

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 # this is flask app
+
+from flask import Flask
+ 
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0",port=5000)
--- test_app.py
+++ test_app.py
@@ -1,1 +1,9 @@
 # Code for unit testing
+
+from app import app
+
+def test_home():     #we should start name with test always
+    response=app.test_client().get("/")
+
+    assert response.status_code==200
+    assert response.data== b"Hello World!"
